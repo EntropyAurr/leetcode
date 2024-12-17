8c3df562a673032cb797c952ffadd53d9ae134c6
Au Cao Thuy Duong <duongau@lantrovn.com>
2024-12-17 14:43:58 +0700

    completed linked lists sorted

--- leetcode.js
+++ leetcode.js
@@ -81,13 +81,63 @@ console.log(isValid("(]"));
 console.log(isValid("([])"));
 
 // Merge Two Sorted Lists
-function mergeTwoLists(list1, list2) {
-  /*   const result = [...list1, ...list2];
-  const sorted = result.sort((a, b) => a - b);
-  return sorted;
-  // the codes above can only be used for plain array */
+class ListNode {
+  constructor(val = 0, next = null) {
+    this.val = val; // assigns the passed-in "val" to the "val" property of the node
+    this.next = next; // assigns the "next" property of the node to the "next" argument, which is a reference to another node
+  }
+}
+// the constructor function runs when creating a new node
+
+function arrayToLinkedList(arr) {
+  if (arr.length === 0) return null;
+
+  // The value -1 is completely arbitrary, it can be any value.
+  // It does not affect the logic because the dummy node is never used as part of the final merged list. It's just a placeholder node at the start of a linked list.
+  const dummy = new ListNode(-1);
+  let current = dummy; // Pointer to traverse and build the list
+
+  for (const val of arr) {
+    current.next = new ListNode(val); // Create a new node with the value
+    current = current.next; // Move to the new node
+  }
+
+  return dummy.next; // Return the head of the list (skip dummy node)
 }
 
-console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]));
-console.log(mergeTwoLists([], []));
-console.log(mergeTwoLists([], [0]));
+const list1 = arrayToLinkedList([1, 2, 4]);
+const list2 = arrayToLinkedList([1, 3, 4]);
+const mergedList = mergeTwoLists(list1, list2);
+
+function mergeTwoLists(list1, list2) {
+  const dummy = new ListNode(-1);
+  let current = dummy;
+
+  while (list1 !== null && list2 !== null) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+
+    current = current.next;
+  }
+
+  current.next = list1 !== null ? list1 : list2;
+  return dummy.next;
+}
+
+function printLinkedList(list) {
+  const result = [];
+
+  while (list !== null) {
+    result.push(list.val);
+    list = list.next;
+  }
+
+  return result;
+}
+
+console.log(printLinkedList(mergedList));
